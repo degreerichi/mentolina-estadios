9e46cb1d45445324e32c4cbbdfa9f0ade9663a90
Rychiv4 <rick_valladares@hotmail.com>
2021-05-12 09:14:37 -0600

    ajustando og values

--- pages/share.js
+++ pages/share.js
@@ -5,12 +5,14 @@ export default function Share() {
    return (
       <>
          <Head>
-            <meta property="og:url" content="https://micasamiestadio.com/share" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="Your Website Title" />
-            <meta property="og:description" content="Your description" />
-            <meta property="og:image" content="https://dummyimage.com/vga" />
+            <meta property="og:url" content="https://micasamiestadio.com/share"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="Your Website Title"/>
+            <meta property="og:description" content="Your description"/>
+            <meta property="og:image" content="https://dummyimage.com/vga"/>
+            <meta property="fb:app_id" content="827394434550474"/>
          </Head>
+         <img src="https://dummyimage.com/vga" alt="" />
       </>
    )
 }
