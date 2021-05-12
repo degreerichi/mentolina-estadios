7d3c03c69a93784d954a9a2b0d957be65ecdc7d0
Rychiv4 <rick_valladares@hotmail.com>
2021-05-12 09:07:01 -0600

    probando el mi casa mi estadio share

--- pages/share.js
+++ pages/share.js
@@ -5,11 +5,11 @@ export default function Share() {
    return (
       <>
          <Head>
-            <meta property="og:url"           content="https://micasamiestadio.com" />
-            <meta property="og:type"          content="website" />
-            <meta property="og:title"         content="Your Website Title" />
-            <meta property="og:description"   content="Your description" />
-            <meta property="og:image"         content="https://dummyimage.com/vga" />
+            <meta property="og:url" content="https://micasamiestadio.com/share" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Your Website Title" />
+            <meta property="og:description" content="Your description" />
+            <meta property="og:image" content="https://dummyimage.com/vga" />
          </Head>
       </>
    )
