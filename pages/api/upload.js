6c70df81f46e01087c0d5b4ebd8b388be53030fa
Rychiv4 <rick_valladares@hotmail.com>
2021-05-12 12:52:34 -0600

    haciendo pruebas con el share image

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,7 +2,15 @@ import firebaseConfig from '../../components/firebaseconfig'
 import firebase from "firebase/app"
 import "firebase/firestore"
 import s3config from '../../components/s3config'
-import S3 from 'aws-s3';
+import S3 from 'aws-s3'
+import { createCanvas, loadImage, registerFont }  from 'canvas'
+import * as fs from 'fs';
+import path from 'path'
+
+registerFont(path.resolve('./public/fonts/MonumentExtended-Regular.ttf'), {
+   family: 'monument',
+   weight: 'bold'
+});
 
 export default function handler(req, res){
 
@@ -17,17 +25,54 @@ export default function handler(req, res){
 
       const newFileName = 'mentolina';
 
-      S3Client
-         .uploadFile(file, newFileName)
-         .then(data => console.log(data))
-         .catch(err => console.error(err))
 
-      let image = {
-         id_registro: req.body.id,
-         tipo: 'imagen_compartida',
-         s3_key: '',
-         s3_location: ''
-      }
+      loadImage('public/media/base.jpg').then((image) => {
+
+         const canvas = createCanvas(1000, 1000);
+         const ctx = canvas.getContext('2d');
+
+         ctx.drawImage(image, 0, 0, 1000, 1000);
+         ctx.fillStyle = '#00ff54';
+         ctx.textAlign = "center";
+         ctx.font = 'italic normal 700 100px 1.1 monument';
+         ctx.fillText('VALLADARES', 490, 760);
+
+         const out = fs.createWriteStream('public/media/test.png')
+         const stream = canvas.createPNGStream();
+         stream.pipe(out)
+         out.on('finish', () =>  console.log('The PNG file was created.'));
+         // canvas.toDataURL();
+
+      });
+
+      // var loadedImage;
+
+      // Jimp.read('public/media/base.jpg').then(img => {
+      //    loadedImage = img;
+      //    return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+      // }).then((font)=>{
+      //    loadedImage.print(font, 180, 750, 'Hello world!');
+      //    loadedImage.write('public/media/base-writed.jpg'); // save
+      // }).catch((err)=>{
+      //    console.log(err);
+      // });
+
+      res.send({
+         result: 'image',
+         message: 'proceso completado, no se sabe de la imagen'
+      });
+
+      // S3Client
+      //    .uploadFile(file, newFileName)
+      //    .then(data => console.log(data))
+      //    .catch(err => console.error(err))
+
+      // let image = {
+      //    id_registro: req.body.id,
+      //    tipo: 'imagen_compartida',
+      //    s3_key: '',
+      //    s3_location: ''
+      // }
 
    }else{
       res.status(404);
--- pages/share.js
+++ pages/share.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { createCanvas, loadImage } from 'canvas'
+
+// registerFont('public/fonts/MonumentExtended-Regular.ttf', {
+//    family: 'monument',
+//    weight: 'bold'
+// });
 
 export default function Share() {
+
+  const router = useRouter();
+  const [theImage, setTheImage] = useState('');
+
+  useEffect(()=>{
+
+      loadImage('/media/base.jpg').then((image) => {
+
+         const canvas = createCanvas(800, 800);
+         const ctx = canvas.getContext('2d');
+
+         ctx.drawImage(image, 0, 0, 800, 800);
+         ctx.fillStyle = '#00ff54';
+         ctx.textAlign = "center";
+         ctx.font = 'italic normal 700 100px 1.1 Arial';
+         ctx.fillText('VALLADARES', 390, 580);
+
+         setTheImage(canvas.toDataURL());
+
+      });
+
+  });
+
+
    return (
       <>
          <Head>
@@ -9,11 +40,11 @@ export default function Share() {
             <meta property="og:type" content="website"/>
             <meta property="og:title" content="Micasamiestadio"/>
             <meta property="og:description" content="Apoyemos a la H compartiendo nuestro propio estadio"/>
-            <meta property="og:image" content="https://dummyimage.com/vga"/>
+            <meta property="og:image" content={theImage}/>
             <meta property="fb:app_id" content="827394434550474"/>
          </Head>
          <div className="share-image-wrapper">
-            <img src="https://dummyimage.com/vga" alt="" />
+            <img src={theImage} alt="" />
          </div>
       </>
    )
