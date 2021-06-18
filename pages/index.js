e69b4c3de95c2a2c457f38f7c0afa8e9c36a3d6a
Rychiv4 <rick_valladares@hotmail.com>
2021-06-18 08:48:29 -0600

    arreglando responsive en modal y agregando pixel de facebook

--- pages/index.js
+++ pages/index.js
@@ -205,6 +205,26 @@ export default function Home({lng, lat}) {
                   gtag('config', 'G-ZFH68VKKRS');
                `
             }}/>
+            {/* <!-- Facebook Pixel Code --> */}
+            <script dangerouslySetInnerHTML={{
+               __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window,document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js';);
+                  fbq('init', '305007251100756'); 
+                  fbq('track', 'PageView');
+               `
+            }}>
+            </script>
+            <noscript>
+            <img height="1" width="1" src="https://www.facebook.com/tr?id=305007251100756&ev=PageView&noscript=1"/>
+            </noscript>
+            {/* <!-- End Facebook Pixel Code --> */}
          </Head>
          <div className="bottom-nav">
             {!headerReduced
@@ -229,13 +249,13 @@ export default function Home({lng, lat}) {
             <h1 className="text-center mb-4">Premios</h1>
             <div className="row">
                <div className="col-12 col-md-4">
-                  <img className="img-fluid" src="/media/premios/camisa.png" alt="" />
+                  <img className="img-fluid img-premio" src="/media/premios/camisa.png" alt="" />
                </div>
                <div className="col-12 col-md-4">
-                  <img className="img-fluid" src="/media/premios/parrillada.png" alt="" />
+                  <img className="img-fluid img-premio" src="/media/premios/parrillada.png" alt="" />
                </div>
                <div className="col-12 col-md-4">
-                  <img className="img-fluid" src="/media/premios/televisor.png" alt="" />
+                  <img className="img-fluid img-premio" src="/media/premios/televisor.png" alt="" />
                </div>
             </div>
             {/* <div className="row">
